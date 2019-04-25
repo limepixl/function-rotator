e1ab42dcbdbf235b65b32f126cdf17c5f5b28c28
Stefan Ivanovski <stefs.ivanovski@gmail.com>
2019-04-25 18:32:03 +0200

    Code cleanup and preparation
    Cleaned up the code and documented it.
    Prepared vertices for upcoming features.

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,10 @@ static bool rotate = false;
 
 int main()
 {
-	// const float TWOPI = 2.0f * 3.1415926f;
-	const float PHI = 1.618033f;
+	// Constants
 	const int WIDTH = 1280;
 	const int HEIGHT = 720;
-	
+
 	// Bounds of function
 	float a, b;
 	std::cout << "Enter the bounds of the function." << std::endl;
@@ -29,16 +28,26 @@ int main()
 	std::cin >> a;
 	std::cout << "Enter a value to end at (b): ";
 	std::cin >> b;
-	
+
+	// Increment size between 2 points on the curve,
+	// AKA distance. Affects concentric circle delta.
+	float incrementSize;
+	std::cout << "Enter the increment size between any 2 X values: " << std::endl;
+	std::cout << "(0 for default, 0.05) ";
+	std::cin >> incrementSize;
+
+	if(incrementSize <= 0.0f)
+		incrementSize = 0.05f;
+
 	// GLFW initialization
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	
+
 	// Window creation
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Title", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Function rotator", nullptr, nullptr);
 	if(window == nullptr)
 	{
 		std::cout << "Failed to create window!" << std::endl;
@@ -54,98 +63,111 @@ int main()
 		std::cout << "Failed to initialize GLEW" << std::endl;
 	}
 	glViewport(0, 0, WIDTH, HEIGHT);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);	// Window background color
 	glEnable(GL_DEPTH_TEST);
-	
-	std::vector<float> vertices;
 
-	float incrementSize = 0.05f;
+	// Generate the function's curve as plotted on a 2D xy plane
+	std::vector<float> vertices;
 	for(float s = a; s<=b; s+=incrementSize)
-	{	
+	{
 		// Insert function here
 		float x = s;
-		float y = std::sin(s);
-		float z = 0.0f;
+		float y = std::sin(std::cos(s));
+		float z = 0.0;
 		vertices.push_back(x);
 		vertices.push_back(y);
 		vertices.push_back(z);
 	}
-	Mesh line(vertices);
-	
-	unsigned int iterations = 50;
-	unsigned int mul = 360/iterations;
+	Mesh curve(vertices);
+
+	// The number of times the points will be rotated
+	// AKA the number of points in each circle at each x value.
+	int iterations = 100;
+
+	// Increment size to rotate by (in degrees)
+	// to reach 360 degrees with the number of
+	// set iterations.
+	float mul = static_cast<float>(360)/iterations;
+
+	// Generate rotated vertices along the circle
+	// defined by the vertex's y coord as the radius.
 	std::vector<float> vertices3D;
 	for(size_t i = 0; i < vertices.size(); i+=3)
 	{
 		glm::vec3 current(vertices[i], vertices[i+1], vertices[i+2]);
-		
+
 		// There are 360 vertices in each row
-		for(float j = 0.0f; j <= iterations; j++)
+		for(int j = 0; j <= iterations; j++)
 		{
 			glm::mat4 rotator(1.0);
+
+			// Store a rotation along the x axis, by mul*j degrees in the matrix
 			rotator = glm::rotate(rotator, glm::radians(mul * j), {1.0, 0.0, 0.0});
-			
-			glm::vec4 rotated = rotator * glm::vec4(current.x, current.y, current.z, 1.0);
-			
+
+			// Rotated vertex
+			glm::vec3 rotated = rotator * glm::vec4(current.x, current.y, current.z, 1.0);
+
 			vertices3D.push_back(rotated.x);
 			vertices3D.push_back(rotated.y);
 			vertices3D.push_back(rotated.z);
 		}
 	}
-	
 	Mesh shape(vertices3D);
-	
+
 	Shader shader("../function-rotator/res/shaders/vertex.glsl", "../function-rotator/res/shaders/fragment.glsl");
 	shader.useProgram();
-	
+
 	glm::mat4 view(1.0);
 	shader.setMat4(view, "view");
-	
+
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), static_cast<float>(WIDTH) / HEIGHT, 0.1f, 50.0f);
 	shader.setMat4(projection, "projection");
-	
+
+	// Number of vertices that make up the initial curve
 	int vertexCount = static_cast<int>(vertices.size() / 3);
 	while(!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+		// Clear window
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
+
 		glm::mat4 model(1.0);
 		model = glm::translate(model, {0.0, 0.0, -10.0});
-		shader.setMat4(model, "model");		
-		
+		shader.setMat4(model, "model");
+
 		if(rotate)
 		{
 			model = glm::rotate(model, glm::radians(25.0f * static_cast<float>(glfwGetTime())), {0.0, 1.0, 0.0});
-			shader.setMat4(model, "model");		
+			shader.setMat4(model, "model");
 		}
-		
+
+		// Draw the 3D generated shape
 		if(show3D)
 		{
 			shape.bind();
-			//glDrawElements(GL_TRIANGLES, shape.vertexCount, GL_UNSIGNED_INT, nullptr);
-			glDrawArrays(GL_POINTS, 0, shape.vertexCount);
+			glDrawArrays(GL_LINE_STRIP, 0, shape.vertexCount);
 			shape.unbind();
-		} 
-		else 
-		{
-			line.bind();
-			glDrawArrays(GL_LINE_STRIP, 0, vertexCount);
-			line.unbind();			
 		}
-	
+		else // Draw the 2D curve
+		{
+			curve.bind();
+			glDrawArrays(GL_LINE_STRIP, 0, vertexCount);
+			curve.unbind();
+		}
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-	
+
+	// Delete the window
 	glfwTerminate();
 	return 0;
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if(key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+	if(key == GLFW_KEY_SPACE && action == GLFW_PRESS)
 		show3D = !show3D;
-	
+
 	if(key == GLFW_KEY_LEFT_SHIFT && action == GLFW_PRESS)
 		rotate = !rotate;
 }
