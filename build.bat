ce34ec1010d0335ed382b443ddedd286d3d826e2
Stefan Ivanovski <stefs.ivanovski@gmail.com>
2019-07-29 11:33:02 +0200

    Updated batch auto closing

--- build.bat
+++ build.bat
@@ -4,4 +4,5 @@ set root=%~dp0
 
 %mingw%g++ %root%src\main.cpp -std=c++11 -O2 %root%src\Mesh.cpp %root%src\Shader.cpp %root%thirdparty\glad\src\glad.c -I %root%thirdparty\glad\include\ -I %root%thirdparty\glfw-mingw\include\ -I %root%thirdparty\glm\ -L %root%thirdparty\glfw-mingw\lib -lglfw3 -lopengl32 -lgdi32 -o function-rotator.exe
 
-echo Build complete! You can now launch %root%function-rotator.exe
+echo Build complete!
+pause
