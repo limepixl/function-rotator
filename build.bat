d44048a9468277ff69eeddcb7524d0321c3c533c
Stefan Ivanovski <stefs.ivanovski@gmail.com>
2019-07-29 11:29:31 +0200

    Removed glm docs and fixed batch file

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
-set mingw = %~dp0thirdparty\ThreadCC\x64\bin
-set root = %~dp0
+set mingw=%~dp0thirdparty\ThreadCC\x64\bin\
+set root=%~dp0
 
-%mingw%g++ .\src\*.cpp .\thirdparty\glad\src\glad.c -I .\thirdparty\glad\include\ -I .\thirdparty\glfw-mingw\include\ -I .\thirdparty\glm\ -L .\thirdparty\glfw-mingw\lib -lglfw3 -lopengl32 -lgdi32 -o function-rotator.exe
+%mingw%g++ %root%src\main.cpp -std=c++11 -O2 %root%src\Mesh.cpp %root%src\Shader.cpp %root%thirdparty\glad\src\glad.c -I %root%thirdparty\glad\include\ -I %root%thirdparty\glfw-mingw\include\ -I %root%thirdparty\glm\ -L %root%thirdparty\glfw-mingw\lib -lglfw3 -lopengl32 -lgdi32 -o function-rotator.exe
+
+echo Build complete! You can now launch %root%function-rotator.exe
