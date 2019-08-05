d7582ed70f437d6ba8f130ec706f31c9e0788ef0
Stefan Ivanovski <stefs.ivanovski@gmail.com>
2019-08-05 15:50:23 +0200

    Added support for debug options in code

--- build-debug.bat
+++ build-debug.bat
@@ -1,7 +1,7 @@
 @echo off
 set root=%~dp0
 
-g++ %root%src\main.cpp -std=c++11 -g3 -Wall -D_CRT_SECURE_NO_WARNINGS %root%src\Mesh.cpp %root%src\Shader.cpp %root%thirdparty\glad\src\glad.c -I %root%thirdparty\glad\include\ -I %root%thirdparty\glfw-mingw\include\ -I %root%thirdparty\glm\ -L %root%thirdparty\glfw-mingw\lib -lglfw3 -lopengl32 -lgdi32 -o function-rotator-debug.exe
+g++ %root%src\main.cpp -std=c++11 -g3 -Wall -D_CRT_SECURE_NO_WARNINGS -D_DEBUG %root%src\Mesh.cpp %root%src\Shader.cpp %root%thirdparty\glad\src\glad.c -I %root%thirdparty\glad\include\ -I %root%thirdparty\glfw-mingw\include\ -I %root%thirdparty\glm\ -L %root%thirdparty\glfw-mingw\lib -lglfw3 -lopengl32 -lgdi32 -o function-rotator-debug.exe
 
 echo Build complete!
 pause
